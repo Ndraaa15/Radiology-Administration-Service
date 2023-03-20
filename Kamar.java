f75c530c6992fae0b67df84b333a96f8d2bbbcbc
Gede Indra Adi Brata <112854205+Ndraaa15@users.noreply.github.com>
2023-03-20 12:07:33 +0700

    Update Kamar.java

--- Kamar.java
+++ Kamar.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package RadiologiFinal;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author ASUS
- */
+
 class Kamar {
     private Integer noKamar;
     static Scanner sc = new Scanner(System.in);
